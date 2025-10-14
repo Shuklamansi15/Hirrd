2c4c57a6642782451bd657de67ed006d85c860db
Shuklamansi15 <shuklamansi15@gmail.com>
2025-10-15 03:15:36 +0530

    main

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,14 @@
-// import path from "path"
-// import tailwindcss from "@tailwindcss/vite"
-// import react from "@vitejs/plugin-react"
-// import { defineConfig } from "vite"
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// })
-
-
-
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    chunkSizeWarningLimit: 1500, // increase from default (500 KB) to e.g. 1500 KB
-  },
-});
+})
