dc2cac326e48a7a8a314c9e2f531f3d2c5ac3086
Shuklamansi15 <shuklamansi15@gmail.com>
2025-10-15 02:17:11 +0530

    final

--- src/components/protected-route.jsx
+++ src/components/protected-route.jsx
@@ -10,6 +10,11 @@ const ProtectedRoute = ({children}) => {
         return <Navigate to="/?sign-in=true"/>
     }
 
+    if(
+        user !== undefined  && !user?.unsafeMetadata?.role && pathname!== "/onboarding"
+    )
+    return <Navigate to= "/onboarding" />
+
   return children;
 }
 
